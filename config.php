8c71fc8f6b919c849b95959e764d87ca54d4bfad
Mihir Patel <ee.mihir.patel@gmail.com>
2020-04-03 00:13:50 -0400

    fix: allow DATABASE_TYPE to be configured

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'pgsql';
+$CFG->dbtype    = getenv('DATABASE_TYPE');
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = getenv('DATABASE_HOST');
 $CFG->dbname    = getenv('DATABASE_NAME');
